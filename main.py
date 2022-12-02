4ace9251c431c6706f0cb2c7772eca0b19d00917
MartinNava <martin.nava1712@gmail.com>
2022-12-01 18:24:44 -0600

    .

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ app.include_router(ModelDatabaseRouter)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=4000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info")
